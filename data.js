4bb1d6422e7236595dae1f2d394098ea6eb463e3
sofa-may <126791639+sofa-may@users.noreply.github.com>
2023-04-27 17:55:07 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+
+export const context = {
+  languages: [
+    {
+      name: 'HTML',
+      modern: true,
+    },
+    {
+      name: 'CSS',
+      modern: true,
+    },
+    {
+      name: 'JavaScript',
+      modern: true,
+    },
+    {
+      name: 'COBOL',
+      modern: false,
+    },
+  ],
+};
+
+
